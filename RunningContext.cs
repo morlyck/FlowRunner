28af1174fef0f55acad7a25e07a6234ac9acac11
峻也 本田 <morlyckbd@gmail.com>
2022-01-29 12:58:25 +0900

    RunningContextをシリアライズ可能な形式に変換する関数の追加

--- RunningContext.cs
+++ RunningContext.cs
@@ -8,15 +8,31 @@ using FlowRunner.LabelRun;
 
 namespace FlowRunner.Engine
 {
-    public class RunningContext : IRunningContext
+
+
+
+    public class RunningContextSdReady
     {
         public bool IsHalting { get; set; } = true;
-        public Dictionary<string, int> Labels { get; set; } = new Dictionary<string, int>();
-        public Statement[] Statements { get; set; } = new Statement[0];
         public int ProgramCounter { get; set; } = 0;
         public string CurrentPackCode { get; set; } = "";
         public Stack<StackFrame> CallStack { get; set; } = new Stack<StackFrame>();
+    }
+    public class RunningContext : RunningContextSdReady,
+   IRunningContext
+    {
+        public RunningContextSdReady GetSdReady() {
+            RunningContextSdReady sdReady = new RunningContextSdReady();
+            sdReady.IsHalting = IsHalting;
+            sdReady.ProgramCounter = ProgramCounter;
+            sdReady.CurrentPackCode = CurrentPackCode;
+            sdReady.CallStack = CallStack;
 
+            return sdReady;
+        }
+
+        public Dictionary<string, int> Labels { get; set; } = new Dictionary<string, int>();
+        public Statement[] Statements { get; set; } = new Statement[0];
         //
         public BuildinCommandExecutionContext BuildinCommandExecutionContext { get; } = new BuildinCommandExecutionContext();
 
@@ -25,4 +41,5 @@ namespace FlowRunner.Engine
             Statements = pack.Statements;
         }
     }
+
 }
