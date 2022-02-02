0f035c27d42975beb3eb4fe32e0f93668f8099bf
峻也 本田 <morlyckbd@gmail.com>
2022-02-02 17:36:17 +0900

    SuspendResumeCycleの追加

---
+++ frCycle/SuspendResumeCycle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Diagnostics;
+
+using FlowRunner.Engine;
+
+namespace FlowRunner
+{
+    public partial class FlowRunnerEngine
+    {
+
+    }
+}
+
+namespace FlowRunner.Engine.Cycle
+{
+    public class SuspendResumeCycle : FlowRunnerCycle
+    {
+        public SuspendResumeCycle(FlowRunnerEngine engine) : base(engine) { }
+
+        //プロパティ
+        //FlowRunnerEngine? engine { get; }
+        //IFlowRunnerService? service { get; }
+        //protected FlowRunner.Engine.Infra.IFlowRunnerInfra? infra { get; }
+
+
+    }
+
+}
---
+++ frCycle/bt/bt_SuspendResumeCycle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using FlowRunner.Engine.Cycle;
+
+//SuspendResumeCycle
+
+namespace FlowRunner
+{
+    public partial class FlowRunnerEngine
+    {
+        SuspendResumeCycle? IFlowRunnerCycle.SuspendResumeCycle { get => suspendResumeCycle; }
+        SuspendResumeCycle? suspendResumeCycle = null;
+
+        [FlowRunnerCycleInitialization]
+        void Initialization_SuspendResumeCycle() {
+            suspendResumeCycle = new SuspendResumeCycle(this);
+
+            //サービスリストに追加
+            flowRunnerCycles.Add(suspendResumeCycle);
+        }
+    }
+}
+
+namespace FlowRunner.Engine.Cycle
+{
+    public partial interface IFlowRunnerCycle
+    {
+        SuspendResumeCycle? SuspendResumeCycle { get; }
+    }
+}
