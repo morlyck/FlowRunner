e7034ba4c425325d5e27257d4edfc723e5581f3e
峻也 本田 <morlyckbd@gmail.com>
2022-02-12 00:01:40 +0900

    スタート・ストップサイクルの追加

---
+++ frCycle/StartStopCycle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Diagnostics;
+
+using FlowRunner.Engine;
+using FlowRunner.Utl;
+
+namespace FlowRunner
+{
+    public partial class FlowRunnerEngine
+    {
+
+    }
+}
+
+namespace FlowRunner.Engine.Cycle
+{
+
+    public class StartStopCycle : FlowRunnerCycle
+    {
+        public StartStopCycle(FlowRunnerEngine engine) : base(engine) { }
+
+        //プロパティ
+        //FlowRunnerEngine? engine { get; }
+        //IFlowRunnerService? service { get; }
+        //protected FlowRunner.Engine.Infra.IFlowRunnerInfra? infra { get; }
+
+        public class StartCycleTimeAttribute : Attribute { }
+        public class StopCycleTimeAttribute : Attribute { }
+
+        public void StartCycle() {
+            engine.Node.StartCycleTimeAll();
+        }
+
+    }
+
+
+}
---
+++ frCycle/bt/bt_StartStopCycle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using FlowRunner.Engine.Cycle;
+
+//StartStopCycle
+
+namespace FlowRunner
+{
+    public partial class FlowRunnerEngine
+    {
+        StartStopCycle? IFlowRunnerCycle.StartStopCycle { get => StartStopCycle; }
+        StartStopCycle? StartStopCycle = null;
+
+        [FlowRunnerCycleInitialization]
+        void Initialization_StartStopCycle() {
+            StartStopCycle = new StartStopCycle(this);
+
+            //サービスリストに追加
+            flowRunnerCycles.Add(StartStopCycle);
+        }
+    }
+}
+
+namespace FlowRunner.Engine.Cycle
+{
+    public partial interface IFlowRunnerCycle
+    {
+        StartStopCycle? StartStopCycle { get; }
+    }
+}
