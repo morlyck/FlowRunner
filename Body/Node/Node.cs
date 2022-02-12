6b5e69f616d07c77fcaf2b278328ce2b6d0129dd
峻也 本田 <morlyckbd@gmail.com>
2022-02-12 16:24:14 +0900

    ノードのローカライズをFuncで行えるように変更

--- Body/Node/Node.cs
+++ Body/Node/Node.cs
@@ -75,25 +75,35 @@ namespace FlowRunner.Engine
         //
         #region(ILabelRunOrdertaker)
         //
+        [Constructor]
+        void Constructor_GetPack() {
+            if (Local_GetPack == null) Local_GetPack = (runner, packCode) => Localize_GetPack(runner, packCode);
+        }
         public Pack GetPack(IRunningContext runningContext, string packCode) {
-            var returnValue = Localize_GetPack(runningContext.Runner, packCode);
+            var returnValue = Local_GetPack(runningContext.Runner, packCode);
             if (returnValue.Item1) return returnValue.Item2;
 
             return NodeOperationRelay(Path).GetPack(runningContext, packCode);
         }
+        public Func<IRunner, string, (bool, Pack)>? Local_GetPack = null;
         protected virtual (bool, Pack) Localize_GetPack(IRunner runner, string packCode) {
             return (false, null);
         }
 
         //
+        [Constructor]
+        void Constructor_Evaluation_ArgumentExpansion() {
+            if (Local_Evaluation_ArgumentExpansion == null) Local_Evaluation_ArgumentExpansion = Localize_Evaluation_ArgumentExpansion;
+        }
         public CommandExecutionContext Evaluation_ArgumentExpansion(IRunningContext runningContext, string commandSymbol, string packCode, string label, string expansionArgumentText) {
-            var returnValue = Localize_Evaluation_ArgumentExpansion(runningContext.Runner, commandSymbol, packCode, label, expansionArgumentText);
+            var returnValue = Local_Evaluation_ArgumentExpansion(runningContext.Runner, commandSymbol, packCode, label, expansionArgumentText);
             if (returnValue.Item1) return returnValue.Item2;
 
             return NodeOperationRelay(Path).Evaluation_ArgumentExpansion(runningContext, commandSymbol, packCode, label, expansionArgumentText);
         }
-            return (false, null);
+        public Func<IRunner, string, string, string, string, (bool, CommandExecutionContext)> Local_Evaluation_ArgumentExpansion = null;
         protected virtual (bool, CommandExecutionContext) Localize_Evaluation_ArgumentExpansion(IRunner runner, string commandSymbol, string packCode, string label, string expansionArgumentText) {
+            return (false, null);
         }
 
         //
@@ -105,6 +115,10 @@ namespace FlowRunner.Engine
                 commands[commandSymbol] = action;
             }
         }
+        [Constructor]
+        void Constructor_ExecutionExpansionCommand() {
+            if (Local_ExecutionExpansionCommand == null) Local_ExecutionExpansionCommand = Localize_ExecutionExpansionCommand;
+        }
         public bool ExecutionExpansionCommand(IRunningContext runningContext, string commandSymbol, CommandExecutionContext commandExecutionContext) {
             //コマンドリストに登録されているなら実行する
             if (commands.ContainsKey(commandSymbol)) {
@@ -113,55 +127,73 @@ namespace FlowRunner.Engine
             }
 
             //ノードオペレーションリレーを使用してコマンドの実行を試みる
-            var returnValue = Localize_ExecutionExpansionCommand(runningContext.Runner, commandSymbol, commandExecutionContext);
+            var returnValue = Local_ExecutionExpansionCommand(runningContext.Runner, commandSymbol, commandExecutionContext);
             if (returnValue) return true;
 
             return NodeOperationRelay(Path).ExecutionExpansionCommand(runningContext, commandSymbol, commandExecutionContext);
         }
-
+        public Func<IRunner, string, CommandExecutionContext, bool> Local_ExecutionExpansionCommand = null;
         protected virtual bool Localize_ExecutionExpansionCommand(IRunner runner, string commandSymbol, CommandExecutionContext commandExecutionContext) {
             return false;
         }
 
         //
+        [Constructor]
+        void Constructor_CatchException_InvalidCommand() {
+            if (Local_CatchException_InvalidCommand == null) Local_CatchException_InvalidCommand = Localize_CatchException_InvalidCommand;
+        }
         public bool CatchException_InvalidCommand(IRunningContext runningContext, InvalidCommandException e) {
-            var returnValue = Localize_CatchException_InvalidCommand(runningContext.Runner, e);
+            var returnValue = Local_CatchException_InvalidCommand(runningContext.Runner, e);
             if (returnValue) return true;
 
             return NodeOperationRelay(Path).CatchException_InvalidCommand(runningContext, e);
         }
-
+        public Func<IRunner, InvalidCommandException, bool> Local_CatchException_InvalidCommand = null;
         protected virtual bool Localize_CatchException_InvalidCommand(IRunner runner, InvalidCommandException e) {
             return false;
         }
 
         //
+        [Constructor]
+        void Constructor_CatchException_LabelResolutionMiss() {
+            if (Local_CatchException_LabelResolutionMiss == null) Local_CatchException_LabelResolutionMiss = Localize_CatchException_LabelResolutionMiss;
+        }
         public bool CatchException_LabelResolutionMiss(IRunningContext runningContext, LabelResolutionMissException e) {
-            var returnValue = Localize_CatchException_LabelResolutionMiss(runningContext.Runner, e);
+            var returnValue = Local_CatchException_LabelResolutionMiss(runningContext.Runner, e);
             if (returnValue) return true;
 
             return NodeOperationRelay(Path).CatchException_LabelResolutionMiss(runningContext, e);
         }
+        public Func<IRunner, LabelResolutionMissException, bool> Local_CatchException_LabelResolutionMiss = null;
         protected virtual bool Localize_CatchException_LabelResolutionMiss(IRunner runner, LabelResolutionMissException e) {
             return false;
         }
 
         //
+        [Constructor]
+        void Constructor_CatchException_ProgramCounterOutOfRange() {
+            if (Local_CatchException_ProgramCounterOutOfRange == null) Local_CatchException_ProgramCounterOutOfRange = Localize_CatchException_ProgramCounterOutOfRange;
+        }
         public bool CatchException_ProgramCounterOutOfRange(IRunningContext runningContext, ProgramCounterOutOfRangeException e) {
-            var returnValue = Localize_CatchException_ProgramCounterOutOfRange(runningContext.Runner, e);
+            var returnValue = Local_CatchException_ProgramCounterOutOfRange(runningContext.Runner, e);
             if (returnValue) return true;
 
             return NodeOperationRelay(Path).CatchException_ProgramCounterOutOfRange(runningContext, e);
         }
+        public Func<IRunner, ProgramCounterOutOfRangeException, bool> Local_CatchException_ProgramCounterOutOfRange = null;
         protected virtual bool Localize_CatchException_ProgramCounterOutOfRange(IRunner runner, ProgramCounterOutOfRangeException e) {
             return false;
         }
 
         #endregion
 
-
+        //シリアライズ
+        [Constructor]
+        void Constructor_Serialize() {
+            if (Local_Serialize == null) Local_Serialize = Localize_Serialize;
+        }
         public string Serialize() {
-            var returnValue = Localize_Serialize(true, null);
+            var returnValue = Local_Serialize(true, null);
 
             bool engineRoot = NodeOperationRelay(Path).Path == "/";
             //sdReady
@@ -185,18 +217,24 @@ namespace FlowRunner.Engine
 
             //ローカルのシリアライズアフター
             if (returnValue.Item1) {
-                return Engine.Infra.GeneralSd.Serialize(Localize_Serialize(false, (sdReady as CustomNodeSdReady)).Item2);
+                return Engine.Infra.GeneralSd.Serialize(Local_Serialize(false, (sdReady as CustomNodeSdReady)).Item2);
             } else {
                 return Engine.Infra.GeneralSd.Serialize((sdReady as CustomNodeSdReady));
             }
 
         }
+        public Func<bool, CustomNodeSdReady, (bool, CustomNodeSdReady)> Local_Serialize = null;
         protected virtual (bool, CustomNodeSdReady) Localize_Serialize(bool beforeSwitch, CustomNodeSdReady sdReady) {
             return (false, null);
         }
 
+        //デシリアライズ
+        [Constructor]
+        void Constructor_Deserialize() {
+            if (Local_Deserialize == null) Local_Deserialize = Localize_Deserialize;
+        }
         public void Deserialize(FlowRunnerEngine engine, string text) {
-            var returnValue = Localize_Deserialize(true, null, engine, text);
+            var returnValue = Local_Deserialize(true, null, engine, text);
             NodeSdReadyCommon sdReady;
             if (returnValue.Item1) {
                 sdReady = returnValue.Item2;
@@ -217,9 +255,9 @@ namespace FlowRunner.Engine
 
 
             //アフター
-            if (returnValue.Item1) Localize_Deserialize(false, (sdReady as CustomNodeSdReady), engine, text);
+            if (returnValue.Item1) Local_Deserialize(false, (sdReady as CustomNodeSdReady), engine, text);
         }
-
+        public Func<bool, CustomNodeSdReady, FlowRunnerEngine, string, (bool, CustomNodeSdReady)> Local_Deserialize = null;
         protected virtual (bool, CustomNodeSdReady) Localize_Deserialize(bool beforeSwitch, CustomNodeSdReady sdReady, FlowRunnerEngine engine, string text) {
             return (false, null);
         }
